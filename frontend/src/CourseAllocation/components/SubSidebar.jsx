d6f626f71b9a58931ddaadc2499c9e7670579133
pamanyuthmika <puthmika@gmail.com>
2025-04-28 14:57:06 +0530

    almost done

--- frontend/src/CourseAllocation/components/SubSidebar.jsx
+++ frontend/src/CourseAllocation/components/SubSidebar.jsx
@@ -62,7 +62,7 @@ export default function SubSidebar() {
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
               </svg>
             </div>
-            Overview
+            Overview & Report
           </Link>
           
           <Link
@@ -78,7 +78,7 @@ export default function SubSidebar() {
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 17v-2m3 2v-4m3 4v-6m2 10H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
               </svg>
             </div>
-            Reports
+            Subject Reports
           </Link>
         </nav>
 
